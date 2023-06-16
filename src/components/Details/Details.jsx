bbb722ddece839f1f6888a657dd7c36f9d5c4d9d
Adam Oberle <oberle.adam@gmail.com>
2023-06-16 12:26:13 -0500

    added button to details page which takes user back to home upon click

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,18 @@
+import { useHistory } from "react-router-dom"
+
 function Details () {
+
+    const history = useHistory();
+
+    const goHome = () => {
+        console.log('Button was clicked, in goHome function');
+        history.push("/")
+    }
 
     return (
         <div>
             <h1>This is a test</h1>
+            <button onClick={goHome}>Home</button>
         </div>
     )
 }
