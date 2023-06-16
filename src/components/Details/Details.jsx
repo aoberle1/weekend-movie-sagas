8b0a549dec7db7e613d682ca3d1e72fe3358b834
Adam Oberle <oberle.adam@gmail.com>
2023-06-16 14:38:30 -0500

    added tester button to details.jsx, value of details variable is returning what we expect

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -11,11 +11,16 @@ function Details () {
         history.push("/")
     }
 
+    // Test function from Tester button, for running console logs to get values of variables
+    const runTest = () => {
+        console.log('details variable is equal to:', details)
+    }
+
     return (
         <div>
             <h1>This is a test</h1>
             <button onClick={goHome}>Home</button>
-            <button onClick={}>Tester</button>
+            <button onClick={runTest}>Tester</button>
         </div>
     )
 }
